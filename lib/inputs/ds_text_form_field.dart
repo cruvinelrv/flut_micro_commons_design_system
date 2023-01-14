b54fb05e6c040e38729413bf4baa51378322d5b5
Vinicius Cruvinel <vinicius.cruvinel@ilia.digital>
2023-01-14 16:43:18 -0300

    refactor: ajustando componente

--- lib/inputs/ds_text_form_field.dart
+++ lib/inputs/ds_text_form_field.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 
 class DSTextFormField extends StatefulWidget {
   final String? label;
+  final TextEditingController? controller;
   final TextInputType? keyboardType;
   const DSTextFormField({
     super.key,
     this.keyboardType,
     this.label,
+    this.controller,
   });
 
   factory DSTextFormField.currency({
     Key? key,
     TextInputType? keyboardType,
     String? label,
+    TextEditingController? controller,
   }) {
     return DSTextFormField(
       key: key,
       label: label,
       keyboardType: keyboardType,
+      controller: controller,
     );
   }
   @override
@@ -28,6 +32,7 @@ class _DSTextFormFieldState extends State<DSTextFormField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: widget.controller,
       keyboardType: widget.keyboardType,
       decoration: InputDecoration(
         labelText: widget.label,
